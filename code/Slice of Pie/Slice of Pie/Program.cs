8c952b26ae0cdff452fcde5a038f172d4017554e
Mikkel Funch <mikkel.funch@gmail.com>
2012-11-22 13:26:50 +0100

    added gitignore and project

---
+++ code/Slice of Pie/Slice of Pie/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Slice_of_Pie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
