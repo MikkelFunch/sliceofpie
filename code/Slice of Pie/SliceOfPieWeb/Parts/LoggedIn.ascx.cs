ee18d342f1c43c63d443acb9a2c2610c3e9b3447
Mikkel Funch <mikkel.funch@gmail.com>
2012-11-29 17:13:13 +0100

    user interfaces

---
+++ code/Slice of Pie/SliceOfPieWeb/Parts/LoggedIn.ascx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SliceOfPieWeb
+{
+    public partial class LoggedIn : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            LabelWelcome.Text = "Welcome " + (String)Session["email"];
+        }
+
+        protected void ButtonLogOut_Click(object sender, EventArgs e)
+        {
+            //log out
+        }
+    }
+}
