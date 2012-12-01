5a7f2a1fad273ff44b69499ca257c54743084db1
Andreas Precht Poulsen <dresen_precht@hotmail.com>
2012-12-01 16:38:03 +0100

    Started working on the web service

---
+++ code/Slice of Pie/WebServiceApplication/Service1.svc.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using Slice_of_Pie;
+
+namespace WebServiceApplication
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    public class Service1 : IService1
+    {
+        public bool AddUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string TestMethod(String s)
+        {
+            return string.Format("Hello {0}", s);
+        }
+
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
